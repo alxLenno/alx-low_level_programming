0fa5a1d554aeafa2fd9daef356108160f66c4f84
alxLenno <lennoxkahati@gmail.com>
2023-09-09 02:03:20 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all base ten digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (48 = 0; i < 58 ; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
