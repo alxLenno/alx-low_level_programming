a5deeda2cc9da203c20078920492e0d6bdebaded
alxLenno <lennoxkahati@gmail.com>
2023-09-12 21:03:30 +0000

    Task-3-islower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+/**
+ * _islower - Check if c in lower case
+ * @c: The alphabet checked
+ * Return: 0 if not or 1 if lower
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+		return (1);
+	return (0);
+}
