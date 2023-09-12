ac558d3878cc2f7b3e2bf56a7b43b25c11b19d2e
alxLenno <lennoxkahati@gmail.com>
2023-09-12 09:40:07 +0000

    Task-0-_putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 65;
+
+	for ( i;i < 97; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
