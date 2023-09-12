4b06948b8a0f54f17b6e99668e277b3bc790d832
alxLenno <lennoxkahati@gmail.com>
2023-09-12 09:54:09 +0000

    Task-0-_putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet - Prints lowercase alphabets
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 65;i < 97;i++)
+	{
+		putchar(i);
+	}
+	putchar('/n');
+
+	return;
+}
