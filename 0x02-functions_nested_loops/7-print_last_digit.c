22a0a17f5046772e483b8734c25f9cba33a065fb
alxLenno <lennoxkahati@gmail.com>
2023-09-13 01:50:44 +0000

    Task 7 last-digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,8 @@ int print_last_digit(int a)
 {
 	if (a < 0)
 	{
-		a = 0 - a;
+		_putchar(-(a % 10) + '0');
+		return -(a % 10);
 	}
 	_putchar((a % 10) + '0');
 	return (a % 10);
---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+	putchar('0'+INT_MIN);
+}
