86014b8e0a2e46399752a5ca9db3475b946bbfbc
alxLenno <lennoxkahati@gmail.com>
2023-09-13 02:08:16 +0000

    Task 8 Hrs Mins Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints hrs min
+ * Retrun: void
+ */
+
+void jack_bauer(void)
+{
+	int min;
+	int hrs;
+
+	while (hrs <= 23)
+	{
+		if (hrs < 10)
+		{
+			_putchar('0');
+			_putchar('0' + hrs);
+		}
+		else
+		{
+			_putchar(hrs + '0');
+		}
+		_putchar(':');
+
+		if (min < 10)
+		{
+			_putchar('0');
+			_putchar('0' + min);
+		}
+		else
+		{
+			_putchar('0' + min);
+		}
+	}
+	min += 1;
+	if (min == 60)
+	{
+		hrs += 1;
+	}
+}
