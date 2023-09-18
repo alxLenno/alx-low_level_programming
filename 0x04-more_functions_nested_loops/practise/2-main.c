b6f53e33bfc38b8700d5a84f5ab0de1b71f390e5
alxLenno <lennoxkahati@gmail.com>
2023-09-18 17:01:25 +0000

    some changes

---
+++ 0x04-more_functions_nested_loops/practise/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the size of a pointer
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int *p;
+
+	printf("Size of pointer: %lu\n", sizeof(p));
+	return (0);
+}
