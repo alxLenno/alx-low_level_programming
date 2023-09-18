9f26bc9d5b80528a35514aa0717d4fcc30fa69e0
alxLenno <lennoxkahati@gmail.com>
2023-09-18 16:14:06 +0000

    added some files

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strlen - returns the length of a string
  * @s: string
@@ -8,12 +9,11 @@
 int _strlen(char *s)
 {
 	int len = 0;
-	
+
 	while (*s != '\0')
 	{
 		len++;
 		s++;
 	}
-
 	return (len);
 }
